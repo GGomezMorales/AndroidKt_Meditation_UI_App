87b724bcdbc303c38bf6388029b7bad84c87d39c
GGomezMorales <gugomog3@gmail.com>
2023-12-28 19:14:03 -0500

    docs: add REAMED.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AndroidKt_Meditation_UI_App
+
+This App is based on the next repository: https://github.com/philipplackner/MeditationUIYouTube
---
+++ app/src/main/java/com/example/meditationuiapp/presentation/ui/theme/Theme.kt
@@ -0,0 +1,16 @@
+package com.example.meditationuiapp.presentation.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MeditationUIAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    MaterialTheme(
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
